fc1b5689b4ce0cb251493c6dd991dfc5ff53030c
Hammed-Waheed <worldwid01@gmail.com>
2023-02-15 13:06:38 +0100

    Porjects cards in progress

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,22 +24,7 @@ export const Projects = () => {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+    }
   ];
 
   return (
@@ -51,7 +36,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>While I was learning a full stack web development course, we had to build these websites as a practice. Most of the projects are vanilla html, css, and some javascript. Some of the projects are using React, React-Bootstrap, and Framer-Motion.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
